73d681cae4443af0b0b47b1c161289706e2e510e
Memokl <86203969+Memokl@users.noreply.github.com>
2026-07-15 22:18:47 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-07-15T19:18:09.170Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-07-15T19:18:39.337Z
+Commit: Fix race condition in async operations
