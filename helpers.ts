4b8553881da8dac5759d8acdc720a1165c3cab04
Memokl <86203969+Memokl@users.noreply.github.com>
2026-07-15 22:20:45 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-07-15T19:20:14.409Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-07-15T19:20:37.705Z
+Commit: Resolve timezone issues in date handling
