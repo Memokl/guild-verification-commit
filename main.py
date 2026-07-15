421627fdff819528fad6cff714c1394efc8db405
Memokl <86203969+Memokl@users.noreply.github.com>
2026-07-15 22:17:07 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-07-15T19:16:51.425Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-07-15T19:17:00.020Z
+Commit: Fix memory leak in event handlers
