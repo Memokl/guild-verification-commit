68865c6f0e5e706783404d73015bb16e0636aa70
Memokl <86203969+Memokl@users.noreply.github.com>
2026-07-15 22:18:16 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-07-15T19:18:07.682Z
-Commit: Add export functionality for reports
+Timestamp: 2026-07-15T19:18:09.170Z
+Commit: Patch API endpoint error handling
