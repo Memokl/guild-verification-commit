db82e6886f3a37ee192d90cdd98ead2f4057abf2
Memokl <86203969+Memokl@users.noreply.github.com>
2026-07-15 22:20:18 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-07-15T19:19:21.833Z
-Commit: Add export functionality for reports
+Timestamp: 2026-07-15T19:20:10.449Z
+Commit: Fix memory leak in event handlers
